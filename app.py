a641fb1e183a070e2d447fffe55362f7c8296d04
Zhou Zheng <204784224@qq.com>
2025-01-09 11:39:42 +0800

    20250109
    代码环境迁移到了cursor

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ import os
 # 设置环境变量
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+"""如果遇到FileNotFoundError: [Errno 2] No such file or directory: '...\\ # 启用本地开发环境.yaml'问题，
+运行以下命令清除遗留的 PY_ENVIRONMENT 系统环境变量：
+Remove-Item Env:PY_ENVIRONMENT
+然后运行以下命令确认清理成功：
+Get-ChildItem Env:PY_ENVIRONMENT
+如果没有输出，说明清理成功。
+"""
 
 def create_app():
     # 创建并启动API后端线程
